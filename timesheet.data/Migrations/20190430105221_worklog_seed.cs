a50970aa28519686446b9ac9463484c08f4e8b10
waqar iqbal <vakar.iqbal@gmail.com>
2019-04-30 10:58:35 +0400

    Updated Seed Data

---
+++ timesheet.data/Migrations/20190430105221_worklog_seed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace timesheet.data.Migrations
+{
+    public partial class Worklog_seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO Worklogs Values(1, 1, '2019-04-29', 2, 1, GetDate())
+                INSERT INTO Worklogs Values(1, 2, '2019-04-29', 1, 1, GetDate())
+                INSERT INTO Worklogs Values(1, 3, '2019-04-29', 2, 1, GetDate())
+                INSERT INTO Worklogs Values(1, 4, '2019-04-29', 2, 1, GetDate())
+                INSERT INTO Worklogs Values(1, 5, '2019-04-29', 2, 1, GetDate())
+                  GO  ");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
